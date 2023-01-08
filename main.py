ec48e0bb221612231e509bdf59767fb1aaab012c
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2023-01-08 11:26:35 +0100

    added csv

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from queue import Queue
 
+import csv
+
+
 class ArchiveTree():
     def __init__(self, *args, **kwargs):
         self.folders = Queue()
@@ -28,31 +31,40 @@ class ArchiveTree():
             items = requests.get(f"https://deprecated.archivesportaleurope.net/web/guest/directory?p_p_id=directory_WAR_Portal&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=directoryTreeAi&nodeId={folder['key']}&countryCode={folder['countryCode']}").json()
             for item in items:
                 self.dispatch(item)
-
-        self.size()
     
     def size(self):
         print("Folder size:", self.folders.qsize())
         print("Detail size:", len(self.details))
 
     def dump_csv(self):
-        id = 28732
-        detail = requests.get(f"https://deprecated.archivesportaleurope.net/web/guest/directory?p_p_id=directory_WAR_Portal&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=aiDetails&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_pos=2&p_p_col_count=3&id={id}")
-        soup = BeautifulSoup(detail.content, "html.parser")
+        with open('emails.csv', 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=["country_code", "institution", "email", "address"], delimiter=";")
+            writer.writeheader()
 
-        emails = soup.select('a[href^=mailto]')
+            for detail in self.details:
+                page = requests.get(f"https://deprecated.archivesportaleurope.net/web/guest/directory?p_p_id=directory_WAR_Portal&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=aiDetails&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_pos=2&p_p_col_count=3&id={detail['aiId']}")
+        
+                soup = BeautifulSoup(page.content, "html.parser")
+                emails = soup.select('a[href^=mailto]')
+                address = soup.find("td", class_="address")
 
-        for email in emails:
-            href=email['href']
-            try:
-                str1, str2 = href.split(':')
-            except ValueError:
-                break
-            print(str2)
+                for email in emails:
+                    href=email['href']
+                    try:
+                        str1, str2 = href.split(':')
+                    except ValueError:
+                        break
+
+                    writer.writerow({"country_code": detail["countryCode"], "institution": detail["title"], "email": str2, "address": address.text.replace("\n", "")})
+
+
+    def extract_emails(self):
+        self.iterate()
+        self.dump_csv()
 
 tree = ArchiveTree()
 
-tree.dump_csv()
+tree.extract_emails()
 
 # code = "AT"
 # country = "country_30" 
